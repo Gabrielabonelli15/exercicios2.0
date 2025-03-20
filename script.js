3a74df443f00aa06457103e6397eb8a8e73b1278
Gabriela Bonelli da Silva <gabriela_silva21@estudante.sesisenai.org.br>
2025-03-20 11:19:13 -0300

    add - primeira questao pronta

---
+++ script.js
@@ -0,0 +1,14 @@
+let n = prompt("Escreva um número")
+ let total = n/2
+ 
+ function VerificarPar(n) {
+ if(n % 2 === 0){
+     console.log("par")
+ }
+ 
+ else{
+     console.log("impar")
+ }
+ }
+ VerificarPar(n)
+ 
