f4529e09b4084f0c47e96d0600b0c07eb3301e26
Gabriela Bonelli da Silva <gabriela_silva21@estudante.sesisenai.org.br>
2025-03-20 12:33:41 -0300

    add - cinco questoes prontas

--- script.js
+++ script.js
@@ -1,24 +1,55 @@
-let n = prompt("Escreva um número")
- let total = n/2
- 
- function VerificarPar(n) {
- if(n % 2 === 0){
-     console.log("par")
- }
- 
- else{
-     console.log("impar")
- }
- }
- VerificarPar(n)
+let n = prompt("Escreva um número");
+let total = n / 2
 
- //divisao
+function VerificarPar(n) {
+    if (n % 2 === 0) {
+        console.log("par");
+    }
 
- function calcular(n1, n2) {
-    return {
-        soma: parseInt(n1 + n2),
-        subtracao: parseInt(n1 - n2),
-        multiplicacao: parseInt(n1 * n2),
-        divisao: parseInt(n1 / n2). 
-    };
+    else {
+        console.log("impar");
+    }
 }
+VerificarPar(n);
+
+//divisao
+
+let n1 = prompt("digite o primeiro número");
+let n2 = prompt("digite o segundo número");
+
+function calcular(n1, n2) {
+    console.log("soma:" + parseInt(n1 + n2));
+    console.log("subtracao:" + parseInt(n1 - n2));
+    console.log("multiplicacao:" + parseInt(n1 * n2));
+    console.log("divisao:" + parseInt(n1 / n2));
+    //fazer verificação da divisão por zero
+}
+
+calcular();
+
+//divisao
+
+let contador = 10;
+
+while (contador >= 1) {
+    console.log(contador);
+    contador--;
+}
+
+//divisao
+function inverterTexto(texto) {
+    console.log(texto.split('').reverse().join(''));
+}
+
+let texto = "icaro";
+
+inverterTexto(texto);
+
+//divisao
+
+let texto = prompt("Digite uma palavra");
+
+function contarCaracteres(str){
+    return str.length
+}
+console.log(contarCaracteres(texto))
